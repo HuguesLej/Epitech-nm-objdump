392b1fe3472d8fd95e0d41e5ccf482c736a79aa5
HuguesLej <hugues.lejeune@epitech.eu>
2024-02-28 23:31:14 +0100

    [ADD] find symbols names and adresses

--- nm/src/open_file.c
+++ nm/src/open_file.c
@@ -7,12 +7,33 @@
 
 #include "nm.h"
 
+char choose_char(unsigned char type, unsigned char bind)
+{
+    unsigned char c;
+
+    (void) type;
+    (void) bind;
+
+    switch (type) {
+        case STT_NOTYPE:
+            c = '?';
+            break;
+        default:
+            c = ' ';
+    }
+
+    if (bind == STB_WEAK && c == '?')
+        c = 'w';
+
+    return c;
+}
+
 bool open_file(const char *str)
 {
     int fd;
     void *buf;
     struct stat s;
-    Elf64_Ehdr *elf;
+    Elf64_Ehdr *ehdr;
 
     fd = open(str, O_RDONLY);
     if (fd == -1)
@@ -23,16 +44,29 @@ bool open_file(const char *str)
         perror("mmap");
         return false;
     }
-    elf = (Elf64_Ehdr *) buf;
-    if (elf->e_type != ET_REL && elf->e_type != ET_EXEC && elf->e_type != ET_DYN)
+    ehdr = (Elf64_Ehdr *) buf;
+    if (ehdr->e_type != ET_REL && ehdr->e_type != ET_EXEC && ehdr->e_type != ET_DYN)
         return false;
-    
 
-    Elf64_Shdr *shdr = (Elf64_Shdr *) (buf + elf->e_shoff);
-    (void) shdr;
 
-    printf("%u\n", elf->e_shnum);
-    printf("%u\n", elf->e_shentsize);
+    Elf64_Shdr *shdr = (Elf64_Shdr *) (buf + ehdr->e_shoff);
+
+    for (unsigned short i = 0; i < ehdr->e_shnum; i++) {
+        if (shdr[i].sh_type == SHT_SYMTAB) {
+            Elf64_Sym *sym = (Elf64_Sym *) (buf + shdr[i].sh_offset);
+            char *tab = (char *) (buf + shdr[shdr[i].sh_link].sh_offset);
+
+            for (unsigned long j = 0; j < (shdr[i].sh_size / sizeof(Elf64_Sym)); j++) {
+                if (sym[j].st_info != STT_FILE) {
+                    printf("%016lx %c %u %s\n",
+                    sym[j].st_value,
+                    choose_char(ELF64_ST_TYPE(sym[j].st_info), ELF64_ST_BIND(sym[j].st_info)),
+                    ELF64_ST_BIND(sym[j].st_info),
+                    tab + sym[j].st_name);
+                }
+            }
+        }
+    }
 
 
     close(fd);
