edcfbb4fc9ef05745433a31ba37297a4d6a2d29c
HuguesLej <hugues.lejeune@epitech.eu>
2024-02-29 20:57:28 +0100

    [ADD] nm: define symbols_t and s_bounds_t

--- nm/include/nm.h
+++ nm/include/nm.h
@@ -14,7 +14,19 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
-
 #include <stdio.h>
+
+typedef struct symbols_s {
+    unsigned long addr;
+    char type;
+    char *name;
+    symbols_t *prev;
+    symbols_t *next;
+} symbols_t;
+
+typedef struct s_bounds_s {
+    symbols_t *start;
+    symbols_t *end;
+} s_bounds_t;
 
 bool open_file(const char *str);
