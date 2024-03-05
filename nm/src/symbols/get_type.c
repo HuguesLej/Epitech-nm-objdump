cce8509fabd82eea96d3f0523db2aa308a219bee
HuguesLej <hugues.lejeune@epitech.eu>
2024-03-05 19:16:48 +0100

    [FIX] coding-style in get_type file

--- nm/src/symbols/get_type.c
+++ nm/src/symbols/get_type.c
@@ -7,45 +7,47 @@
 
 #include "nm.h"
 
+static char find_type(char c, unsigned short st_shndx, unsigned sh_type,
+    unsigned long sh_flags)
+{
+    if (sh_type == STT_NOTYPE)
+        return 'U';
+    if (st_shndx == SHN_ABS)
+        return 'a';
+    if (st_shndx == SHN_COMMON)
+        return 'c';
+    if (sh_type == SHT_NOBITS && sh_flags == (SHF_ALLOC | SHF_WRITE))
+        return 'b';
+    if ((sh_type == SHT_PROGBITS || sh_type == SHT_FINI_ARRAY
+        || sh_type == SHT_DYNAMIC || sh_type == SHT_INIT_ARRAY)
+        && sh_flags == (SHF_ALLOC | SHF_WRITE))
+            return 'd';
+    if ((sh_type == SHT_PROGBITS || sh_type == SHT_NOTE)
+        && sh_flags == SHF_ALLOC)
+            return 'r';
+    if (sh_type == SHT_PROGBITS && sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
+        return 't';
+    return c;
+}
+
 char get_type(Elf64_Sym *sym, unsigned sh_type, unsigned long sh_flags)
 {
     char c = '?';
     unsigned char st_type = ELF64_ST_TYPE(sym->st_info);
     unsigned char st_bind = ELF64_ST_BIND(sym->st_info);
+    bool is_up;
 
-
-    if (st_type == STB_GNU_UNIQUE) {
+    if (st_type == STB_GNU_UNIQUE)
         c = 'u';
-    }
-
-    if (st_bind == STB_WEAK && st_type == STT_OBJECT) {
+    if (st_bind == STB_WEAK && st_type == STT_OBJECT)
         c = 'v';
-    } else if (st_bind == STB_WEAK) {
+    if (st_bind == STB_WEAK)
         c = 'w';
-    } else if (sh_type == STT_NOTYPE) {
-        c = 'U';
-    } else if (sym->st_shndx == SHN_ABS) {
-        c = 'a';
-    } else if (sym->st_shndx == SHN_COMMON) {
-        c = 'c';
-    } else if (sh_type == SHT_NOBITS && sh_flags == (SHF_ALLOC|SHF_WRITE)) {
-        c = 'b';
-    } else if ((sh_type == SHT_PROGBITS || sh_type == SHT_FINI_ARRAY || sh_type == SHT_DYNAMIC || sh_type == SHT_INIT_ARRAY) && sh_flags == (SHF_ALLOC|SHF_WRITE)) {
-        c = 'd';
-    } else if ((sh_type == SHT_PROGBITS || sh_type == SHT_NOTE) && sh_flags == SHF_ALLOC) {
-        c = 'r';
-    } else if (sh_type == SHT_PROGBITS && sh_flags == (SHF_ALLOC | SHF_EXECINSTR)) {
-        c = 't';
-    }
-
-    if ((c == 'w' || c == 'v') && sym->st_shndx != SHN_UNDEF) {
+    c = find_type(c, sym->st_shndx, sh_type, sh_flags);
+    if ((c == 'w' || c == 'v') && sym->st_shndx != SHN_UNDEF)
         c = char_to_uppercase(c);
-    }
-
-    if (st_bind == STB_GLOBAL && !is_char_uppercase(c) && c != '?') {
+    is_up = is_char_uppercase(c);
+    if (st_bind == STB_GLOBAL && !is_up && c != '?')
         c = char_to_uppercase(c);
-    }
-
-
     return c;
 }
